8ffb5a4794583c9b5a766692b4ff8acdcf6c3c52
Julian V <jvesper95@gmail.com>
2021-12-09 15:03:47 +0100

    Add solution for day 1 part 1

--- src/bin/day_1/main.rs
+++ src/bin/day_1/main.rs
@@ -1,3 +1,32 @@
+use helper::lines_from_file;
+
 fn main() {
-    println!("Hello, world!");
+	// Part One
+	// Read file and convert to u32 vector
+    let unparsed_input = lines_from_file("src/bin/day_1/day_1.txt");
+	let depths: Vec<u32> = unparsed_input.iter()
+		.filter_map(|line| line.parse::<u32>().ok())
+		.collect();
+	
+	assert_eq!(depths.len(), 2000);
+
+	// Count number of measurement increases
+	let mut number_of_increases = 0;
+	let mut previous_depth: Option<u32> = None;
+	for depth in depths {
+		match previous_depth {
+			Some(d) => {
+				if d < depth {
+					number_of_increases += 1;
+				}
+				previous_depth = Some(depth);
+			},
+			None => {
+				previous_depth = Some(depth);
+			}
+		}
+		
+	}
+
+	println!("Number of measurement increases: {}", number_of_increases);
 }
