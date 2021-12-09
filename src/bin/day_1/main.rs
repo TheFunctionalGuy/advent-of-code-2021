57ef3e46ad6d6a05a77b901c83380ec08d0f47bb
Julian V <jvesper95@gmail.com>
2021-12-09 16:02:14 +0100

    Add solution for day 2 part 1

--- src/bin/day_1/main.rs
+++ src/bin/day_1/main.rs
@@ -3,7 +3,7 @@ use helper::lines_from_file;
 fn main() {
 	// --- Part One ---
 	// Read file and convert to u32 vector
-    let unparsed_input = lines_from_file("src/bin/day_1/day_1.txt");
+	let unparsed_input = lines_from_file("src/bin/day_1/day_1.txt");
 	let depths: Vec<u32> = unparsed_input.iter()
 		.filter_map(|line| line.parse::<u32>().ok())
 		.collect();
@@ -26,11 +26,10 @@ fn main() {
 				previous_depth = Some(*depth);
 			}
 		}
-		
 	}
 
 	println!("Number of measurement increases: {}", number_of_increases);
-	
+
 
 	// --- Part Two ---
 	number_of_increases = 0;
@@ -38,13 +37,13 @@ fn main() {
 
 	for i in 0..(depths.len() - 2) {
 		let window_sum = depths[i] + depths[i+1] + depths[i+2];
-		
+
 		match previous_window_sum {
 			Some(s) => {
 				if s < window_sum {
 					number_of_increases += 1;
 				}
-				previous_window_sum = Some(window_sum); 
+				previous_window_sum = Some(window_sum);
 			}
 			None => {
 				previous_window_sum = Some(window_sum);
--- src/bin/day_2/main.rs
+++ src/bin/day_2/main.rs
@@ -1,4 +1,66 @@
+use helper::lines_from_file;
+
 fn main() {
 	// --- Part One ---
-	println!("Not yet implemented!");
+	// Read file and parse commands to tuples
+	let unparsed_input = lines_from_file("src/bin/day_2/day_2.txt");
+	let commands = parse_commands(&unparsed_input);
+
+	assert_eq!(commands.len(), 1000);
+
+	let mut horizontal_position: u32 = 0;
+	let mut depth: u32 = 0;
+
+	// Execute all commands
+	for (command, unit) in commands {
+		match command {
+			Command::Forward => {
+				horizontal_position += unit;
+			},
+			Command::Down => {
+				depth += unit;
+			},
+			Command::Up => {
+				depth -= unit;
+			}
+		}
+	}
+
+	println!("Depth: {}", depth);
+	println!("Horizontal position: {}", horizontal_position);
+	println!("Final product: {}", depth * horizontal_position);
+}
+
+enum Command {
+	Forward,
+	Down,
+	Up,
+}
+
+// Parse commands by unpacking strings to tuples of command and unit
+fn parse_commands(unparsed_commands: &Vec<String>) -> Vec<(Command, u32)> {
+	let parsed_commands = unparsed_commands.iter()
+		.filter_map(|command_string| fetch_command(command_string))
+		.collect();
+
+	parsed_commands
+}
+
+// Fetch a single command by splitting string into tuple of command and unit
+fn fetch_command(command_string: &String) -> Option<(Command, u32)> {
+	let splits: Vec<_> = command_string.split(" ").collect();
+
+	let command: Option<Command> = match splits[0] {
+		"forward" => Some(Command::Forward),
+		"down" => Some(Command::Down),
+		"up" => Some(Command::Up),
+		_ => None
+	};
+
+	let unit: u32 = splits[1].parse().unwrap();
+
+	match command {
+		Some(c) => Some((c, unit)),
+		None => None
+	}
 }
