30076d0a159c537206114a1ff008d0230c65cb1d
Julian V <jvesper95@gmail.com>
2021-12-24 10:33:30 +0100

    Add solution for day 6 part 2

--- src/bin/day_6/main.rs
+++ src/bin/day_6/main.rs
@@ -4,13 +4,13 @@ fn main () {
 	// --- Part One ---
 	println!("--- Part One ---");
 	// Read file and convert to vector u32 value
-	let unparsed_input = lines_from_file("src/bin/day_6/test.txt");
+	let unparsed_input = lines_from_file("src/bin/day_6/day_6.txt");
 	let initial_state: Vec<u32> = unparsed_input[0].split(",")
 		.filter_map(|n| n.parse().ok())
 		.collect();
 
 	// Ensure all numbers have been parsed correctly
-	assert_eq!(5, initial_state.len());
+	assert_eq!(300, initial_state.len());
 
 	let mut current_fish_timers = initial_state.clone();
 
@@ -37,26 +37,25 @@ fn main () {
 
 	// --- Part Two ---
 	println!("\n--- Part Two ---");
-	// TODO: Add solution which does not use lists but instead precalculates when new fishes get added
-	current_fish_timers = initial_state;
+	let mut current_fish_timers: [u64; 9] = [0; 9];
 
-	// Simulate fish population for 80 days
+	// Add fishes to initial bins
+	for value in &initial_state {
+		current_fish_timers[*value as usize] += 1;
+	}
+
+	// Simulate fish population for 256 days
 	for day in 0..256 {
-		let mut new_fish: Vec<u32> = Vec::new();
-
-		// Update every fish
-		for i in 0..current_fish_timers.len() {
-			if current_fish_timers[i] == 0 {
-				current_fish_timers[i] = 6;
-				// Create new fish with timer of 8
-				new_fish.push(8);
-			} else {
-				current_fish_timers[i] -= 1;
-			}
+		let new_fish = current_fish_timers[0];
+		
+		// Update every fish bin
+		for i in 0..8 {
+			current_fish_timers[i] = current_fish_timers[i + 1];
 		}
-
-		// Add new fishes to group
-		current_fish_timers.append(&mut new_fish);
-		println!("Number of fishes after {:2} day(s): {}", day, current_fish_timers.iter().count());
+		
+		// Add new fishes to group of 6 and 8
+		current_fish_timers[6] += new_fish;
+		current_fish_timers[8] = new_fish;
+		println!("Number of fishes after {:2} day(s): {}", day, current_fish_timers.iter().sum::<u64>());
 	}
 }
