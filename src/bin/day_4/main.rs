a886b16e4d7eb9848edc6cfb1d8f4305970c9872
Julian V <jvesper95@gmail.com>
2021-12-24 05:56:49 +0100

    Add solution for day 4 part 1

--- src/bin/day_4/main.rs
+++ src/bin/day_4/main.rs
@@ -1,31 +1,157 @@
+use std::fmt;
+
 use helper::lines_from_file;
+
+const BOARD_SIZE: usize = 5;
 
 fn main() {
 	// --- Part One ---
 	println!("--- Part One ---");
 	// Read file and convert to u32 vector and 2d-vector for boards
 	let unparsed_input = lines_from_file("src/bin/day_4/day_4.txt");
-	//let mut state = [[0u8; 4]; 6];
 	let unparsed_numbers = &unparsed_input[0];
-
-	let numbers: Vec<u32> = unparsed_numbers.split(",")
-		.filter_map(|number| number.parse().ok())
+	let unparsed_boards: Vec<&String> = unparsed_input[2..].iter()
+		.filter(|s| !s.is_empty())
 		.collect();
-	
-	for number in &numbers {
-		println!("{}", number);
-	}
 
-	for (index, line) in unparsed_input.iter().enumerate() {
-		println!("{:03}: {}", index, line);
+	// Ensure that number of lines is dividble by the board size
+	assert!(unparsed_boards.len() % BOARD_SIZE == 0);
+
+	// Extract numbers to pull
+	let numbers: Vec<u32> = unparsed_numbers.split(",")
+		.filter_map(|n| n.parse().ok())
+		.collect();
+
+	// Extract boards
+	let mut boards: Vec<BingoBoard> = unparsed_boards.chunks(BOARD_SIZE)
+		.map(|c| create_board(c, BOARD_SIZE))
+		.collect();
+
+	// Pull numbers and check for winner
+	for number in numbers {
+		println!("Now marking number: {}", number);
+
+		// Mark number on all boards
+		boards = boards.iter_mut()
+			.map(|b| mark_number(b, number))
+			.collect();
+
+		// Get winner
+		if let Some(winner) = boards.iter().find(|b| has_won(b)) {
+			println!("Match!");
+
+			let sum: u32 = winner.state.iter()
+				.map(|l| l.iter()
+					.filter_map(|e| match e {
+						BingoEntry::Entry(n) => Some(*n),
+						_ => None,
+					}).sum::<u32>()
+				).sum();
+
+			println!("Board sum: {}", sum);
+			println!("Final result: {}", sum * number);
+
+			break;
+		}
 	}
 }
 
-fn create_board(board_string: &Vec<String>) {
-
+enum BingoEntry {
+	Entry(u32),
+	Hit,
 }
 
-trait BingoBoard {
-	fn show(&self) -> String;
+// Representation of Entry
+impl fmt::Display for BingoEntry {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			// TODO: Change this ugly formatting
+			BingoEntry::Entry(n) => write!(f, "{:^3}", n),
+			BingoEntry::Hit => write!(f, " X ")
+		}
+    }
+}
 
+struct BingoBoard {
+	state: Vec<Vec<BingoEntry>>,
+	size: usize,
+}
+
+// Representation of Board
+impl fmt::Display for BingoBoard {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		// Iterator over all Entries
+		let formatted_string: String = self.state.iter()
+			.map(|l| l.iter()
+				.map(|e| format!("{}", e))
+				.collect::<Vec<String>>()
+				.join(" ")
+			)
+			.collect::<Vec<String>>()
+			.join("\n");
+
+		write!(f, "{}", formatted_string)
+	}
+}
+
+// Create a board from a reference to (multiple) lines which represent a board
+fn create_board(board_string: &[&String], size: usize) -> BingoBoard {
+	assert_eq!(BOARD_SIZE, board_string.len());
+
+	let state: Vec<Vec<BingoEntry>> = board_string.iter()
+		.map(|l| l.split(" ")
+			.filter_map(|n| match n.parse() {
+				Ok(n) => Some(BingoEntry::Entry(n)),
+				Err(_) => None,
+			}).collect())
+		.collect();
+
+	BingoBoard {
+		state,
+		size,
+	}
+}
+
+// Mark number on given board
+fn mark_number(board: &mut BingoBoard, number: u32) -> BingoBoard {
+	BingoBoard {
+		state: board.state.iter_mut()
+			.map(|l| l.into_iter()
+				.map(|e| match e {
+					BingoEntry::Entry(n) => {
+						if *n == number {
+							BingoEntry::Hit
+						} else {
+							BingoEntry::Entry(*n)
+						}
+					},
+					BingoEntry::Hit => BingoEntry::Hit
+				}).collect::<Vec<BingoEntry>>())
+			.collect::<Vec<Vec<BingoEntry>>>(),
+		size: board.size,
+	}
+}
+
+// Returns true if given bingo board has won
+fn has_won(board: &BingoBoard) -> bool {
+	// Mark all boards in the beginning
+	let mut rows: Vec<bool> = vec![true; board.size];
+	let mut columns: Vec<bool> = vec![true; board.size];
+
+	// Remove every row and column containing a "Miss" entry
+	for (i, row) in board.state.iter().enumerate() {
+		for (j, entry) in row.iter().enumerate() {
+			if let BingoEntry::Entry(_) = entry {
+				rows[i] = false;
+				columns[j] = false;
+			}
+		}
+	}
+
+	// When any row is still marked it only contains Hit entries
+	if rows.iter().any(|&v| v) || columns.iter().any(|&v| v) {
+		true
+	} else {
+		false
+	}
 }
