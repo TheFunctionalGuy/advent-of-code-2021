156af52ffe62eeb749acea3cd8182da8f97f2f3f
Julian V <jvesper95@gmail.com>
2021-12-24 06:49:34 +0100

    Add solution for day 4 part 2

--- src/bin/day_4/main.rs
+++ src/bin/day_4/main.rs
@@ -27,8 +27,13 @@ fn main() {
 		.map(|c| create_board(c, BOARD_SIZE))
 		.collect();
 
-	// Pull numbers and check for winner
-	for number in numbers {
+	let mut winner_found = false;
+	let mut winners_index: Vec<usize> = Vec::new();
+	let mut last_number: Option<u32> = None;
+
+	// Pull numbers and check for winner (also contains part two)
+	for number in &numbers {
+		last_number = Some(*number);
 		println!("Now marking number: {}", number);
 
 		// Mark number on all boards
@@ -36,23 +41,53 @@ fn main() {
 			.map(|b| mark_number(b, number))
 			.collect();
 
-		// Get winner
-		if let Some(winner) = boards.iter().find(|b| has_won(b)) {
-			println!("Match!");
+		// Get winner (part one)
+		if !winner_found {
+			if let Some(winner) = boards.iter().find(|b| has_won(b)) {
+				winner_found = true;
+				println!("Winner found!");
 
-			let sum: u32 = winner.state.iter()
-				.map(|l| l.iter()
-					.filter_map(|e| match e {
-						BingoEntry::Entry(n) => Some(*n),
-						_ => None,
-					}).sum::<u32>()
-				).sum();
+				let sum: u32 = winner.state.iter()
+					.map(|l| l.iter()
+						.filter_map(|e| match e {
+							BingoEntry::Entry(n) => Some(*n),
+							_ => None,
+						}).sum::<u32>()
+					).sum();
 
-			println!("Board sum: {}", sum);
-			println!("Final result: {}", sum * number);
+				println!("First board sum: {}", sum);
+				println!("Result (part one): {}", sum * number);
+			}
+		}
 
+		// Mark winners
+		for (index, board) in boards.iter().enumerate() {
+			if has_won(board) {
+				if !winners_index.contains(&index) {
+					winners_index.push(index);
+				}
+			}
+		}
+
+		// Stop early when everyone has won
+		if winners_index.len() == boards.len() {
 			break;
 		}
+	}
+
+	if let Some(last_winner_index) = winners_index.pop() {
+		let sum: u32 = boards[last_winner_index].state.iter()
+		.map(|l| l.iter()
+			.filter_map(|e| match e {
+				BingoEntry::Entry(n) => Some(*n),
+				_ => None,
+			}).sum::<u32>()
+		).sum();
+
+		// --- Part Two ---
+		println!("\n--- Part Two ---");
+		println!("Last board sum: {}", sum);
+		println!("Result (part two): {}", sum * last_number.unwrap());
 	}
 }
 
@@ -113,13 +148,13 @@ fn create_board(board_string: &[&String], size: usize) -> BingoBoard {
 }
 
 // Mark number on given board
-fn mark_number(board: &mut BingoBoard, number: u32) -> BingoBoard {
+fn mark_number(board: &mut BingoBoard, number: &u32) -> BingoBoard {
 	BingoBoard {
 		state: board.state.iter_mut()
 			.map(|l| l.into_iter()
 				.map(|e| match e {
 					BingoEntry::Entry(n) => {
-						if *n == number {
+						if n == number {
 							BingoEntry::Hit
 						} else {
 							BingoEntry::Entry(*n)
