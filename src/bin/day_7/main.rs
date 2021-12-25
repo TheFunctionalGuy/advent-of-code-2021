77403b3c4659ad7acb1ef99edbead5f383734048
Julian V <jvesper95@gmail.com>
2021-12-25 05:31:54 +0100

    Add solution for day 7 part 1

--- src/bin/day_7/main.rs
+++ src/bin/day_7/main.rs
@@ -1,3 +1,42 @@
+use std::cmp::{max, min};
+
+use helper::lines_from_file;
+
 fn main() {
-	
+	// --- Part One ---
+	println!("--- Part One ---");
+	// Read file and convert to vector u32 value
+	let unparsed_input = lines_from_file("src/bin/day_7/day_7.txt");
+	let horizontal_positions: Vec<u32> = unparsed_input[0].split(",")
+		.filter_map(|n| n.parse().ok())
+		.collect();
+
+	// Ensure all numbers have been parsed correctly
+	assert_eq!(1000, horizontal_positions.len());
+
+	// Get highest posible crab position
+	let max_position = horizontal_positions.iter().max().unwrap();
+
+	println!("Highest horizontal position: {}", max_position);
+
+	// Calculate all fuel costs
+	let fuel_costs: Vec<u32> = (0..=*max_position).into_iter()
+		.map(|p| horizontal_positions.iter()
+			.map(|pos| distance(&p, pos))
+			.sum())
+		.collect();
+
+	for (i, fuel_cost) in fuel_costs.iter().enumerate() {
+		println!("Aligning at position {} would cost {} fuel", i, fuel_cost);
+	}
+
+	// Print minimal fuel cost
+	if let Some((index, _)) = fuel_costs.iter().enumerate().min_by(|&(_, x), &(_, y)| x.cmp(y)) {
+		println!("Index for minimal fuel cost is : {}", index);
+		println!("Minimal fuel cost is: {}", fuel_costs[index]);
+	}
+}
+
+fn distance(a: &u32, b: &u32) -> u32 {
+	max(a, b) - min(a, b)
 }
