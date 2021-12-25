b8bf017d370ae8a6838725d15bc30cd1032ca104
Julian V <jvesper95@gmail.com>
2021-12-25 05:47:53 +0100

    Add solution for day 7 part 2

--- src/bin/day_7/main.rs
+++ src/bin/day_7/main.rs
@@ -32,8 +32,30 @@ fn main() {
 
 	// Print minimal fuel cost
 	if let Some((index, _)) = fuel_costs.iter().enumerate().min_by(|&(_, x), &(_, y)| x.cmp(y)) {
-		println!("Index for minimal fuel cost is : {}", index);
+		println!("Index for minimal fuel cost is: {}", index);
 		println!("Minimal fuel cost is: {}", fuel_costs[index]);
+	}
+
+
+	// --- Part Two ---
+	println!("\n--- Part Two ---");
+
+	// Calculate fuel cost according to new rules
+	let new_fuel_costs: Vec<u32> = (0..=*max_position).into_iter()
+		.map(|p| horizontal_positions.iter()
+			.map(|pos| distance(&p, pos))
+			.map(|c| (c * (c + 1)) / 2)
+			.sum())
+		.collect();
+
+	for (i, fuel_cost) in new_fuel_costs.iter().enumerate() {
+		println!("Aligning at position {} would cost {} fuel", i, fuel_cost);
+	}
+
+	// Print new minimal fuel cost
+	if let Some((index, _)) = new_fuel_costs.iter().enumerate().min_by(|&(_, x), &(_, y)| x.cmp(y)) {
+		println!("Index for minimal fuel cost is: {}", index);
+		println!("Minimal fuel cost is: {}", new_fuel_costs[index]);
 	}
 }
 
