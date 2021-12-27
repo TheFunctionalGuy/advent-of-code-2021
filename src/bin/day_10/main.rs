57b4015db15b3787331ac7ba709aeb8cb2c29460
Julian V <jvesper95@gmail.com>
2021-12-27 13:16:31 +0100

    Add solution for day 10 part 2

--- src/bin/day_10/main.rs
+++ src/bin/day_10/main.rs
@@ -9,15 +9,52 @@ fn main() {
 	let navigation_subsystem: Vec<Vec<char>> = unparsed_input.iter()
 		.map(|l| l.chars().collect())
 		.collect();
-	
+
 	// Ensure all numbers have been parsed correctly
 	assert_eq!(98, navigation_subsystem.len());
 
-	let syntax_error_scores: Vec<u32> = navigation_subsystem.iter()
-		.filter_map(|l| check_syntax(l))
-		.collect();
-	
+	// Find all syntax errors
+	let mut syntax_error_scores: Vec<u32> = Vec::new();
+	let mut incomplete_lines: Vec<Vec<char>> = Vec::new();
+
+	for line in navigation_subsystem {
+		if let Some(score) = check_syntax(&line) {
+			syntax_error_scores.push(score);
+		} else {
+			incomplete_lines.push(line);
+		}
+	}
+
 	println!("Total syntax error score: {}", syntax_error_scores.iter().sum::<u32>());
+
+
+	// --- Part Two ---
+	println!("\n--- Part Two ---");
+	let missing_brackets: Vec<Vec<char>> = incomplete_lines.iter()
+		.map(|l| complete_line(l))
+		.collect();
+
+	let mut autocomplete_scores: Vec<u64> = missing_brackets.iter()
+		.map(|v| v.iter()
+			.fold(0, |score, bracket| {
+				match bracket {
+					')' => score * 5 + 1,
+					']' => score * 5 + 2,
+					'}' => score * 5 + 3,
+					'>' => score * 5 + 4,
+					_ => score,
+				}
+			}))
+		.collect();
+
+	assert!(autocomplete_scores.len() % 2 == 1);
+
+	// Sort score
+	autocomplete_scores.sort();
+
+	// Calculate index
+	let index: usize = autocomplete_scores.len() / 2;
+	println!("Middle autocomplete score: {}", autocomplete_scores[index]);
 }
 
 fn check_syntax(line: &Vec<char>) -> Option<u32> {
@@ -54,4 +91,29 @@ fn check_syntax(line: &Vec<char>) -> Option<u32> {
 	}
 
 	None
+}
+
+fn complete_line(line: &Vec<char>) -> Vec<char> {
+	let mut bracket_stack: Vec<char> = Vec::new();
+	let mut missing_brackets: Vec<char> = Vec::new();
+
+	for bracket in line {
+		if ['(', '[', '{', '<'].contains(bracket) {
+			bracket_stack.push(*bracket);
+		} else {
+			bracket_stack.pop();
+		}
+	}
+
+	while let Some(bracket) = bracket_stack.pop() {
+		match bracket {
+			'(' => missing_brackets.push(')'),
+			'[' => missing_brackets.push(']'),
+			'{' => missing_brackets.push('}'),
+			'<' => missing_brackets.push('>'),
+			_ => {},
+		}
+	}
+
+	missing_brackets
 }
